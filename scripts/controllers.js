27b3683c85181e4e64bb1bebf0266ffe07b308b2
YoelMedrano <39170047+YoelMedrano@users.noreply.github.com>
2018-06-14 09:43:44 -0400

    Update controllers.js

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -17,7 +17,7 @@ angular.module('angularRestfulAuth')
                 } else {
                    // window.localStorage.setItem('token',res.data.token);
                     $localStorage.token = res.token;
-                    window.location = "/";    
+                    window.location = "/userprofile";    
                 }
             }, function() {
                 $scope.error = 'Failed to signin';
