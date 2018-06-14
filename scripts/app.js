24115b1b358ba670f476690f7382f9d5acfd2fc4
YoelMedrano <39170047+YoelMedrano@users.noreply.github.com>
2018-06-14 10:42:52 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -30,14 +30,14 @@ angular.module('angularRestfulAuth', [
 
     $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
             return {
-                'request': function (config) {
+                request: function (config) {
                     config.headers = config.headers || {};
                     if ($localStorage.token) {
                         config.headers.Authorization = 'Bearer ' + $localStorage.token;
                     }
                     return config;
                 },
-                'responseError': function(response) {
+                responseError: function(response) {
                     if(response.status === 401 || response.status === 403) {
                         $location.path('/login');
                     }
