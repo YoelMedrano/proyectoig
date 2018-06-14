9d56185b7454d06839c13c485d0153112592c659
YoelMedrano <39170047+YoelMedrano@users.noreply.github.com>
2018-06-14 10:57:56 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -28,24 +28,23 @@ angular.module('angularRestfulAuth', [
             redirectTo: '/'
         });
 
-    app.factory('BearerAuthInterceptor', function ($window, $q) {
-    return {
-        request: function(config) {
-            config.headers = config.headers || {};
-            if ($window.localStorage.getItem('token')) {
-              // may also use sessionStorage
-                config.headers.Authorization = 'Bearer ' + $window.localStorage.getItem('token');
-            }
-            return config || $q.when(config);
-        },
-        response: function(response) {
-            if (response.status === 401) {
-                //  Redirect user to login page / signup Page.
-            }
-            return response || $q.when(response);
-        }
-    };
-});
+    $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
+            return {
+                'request': function (config) {
+                    config.headers = config.headers || {};
+                    if ($localStorage.token) {
+                        config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                    }
+                    return config;
+                },
+                'responseError': function(response) {
+                    if(response.status === 401 || response.status === 403) {
+                        $location.path('/login');
+                    }
+                    return $q.reject(response);
+                }
+            };
+        }]);
 
     }
 ]);
