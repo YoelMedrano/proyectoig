95b8eb0fc23ae94bf867b90890443dbb9054fea2
YoelMedrano <39170047+YoelMedrano@users.noreply.github.com>
2018-07-03 09:48:48 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -48,7 +48,7 @@ angular.module('angularRestfulAuth', [
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in
             if ($location.path() !== '/login' && !$rootScope.globals.currentUser ) {
-                window.location="#/login";
+                window.location="#/";
                 
             }
         });
