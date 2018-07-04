c831179c1c28823d1678de677eba31ce25f34b97
YoelMedrano <39170047+YoelMedrano@users.noreply.github.com>
2018-07-03 20:08:49 -0400

    Update controller.js

--- services/login/controller.js
+++ services/login/controller.js
@@ -13,7 +13,7 @@ angular.module('Authentication')
             AuthenticationService.Login($scope.email, $scope.password, function(response) {
                 if(response.success) {
                     AuthenticationService.SetCredentials($scope.email, $scope.password);
-                    $location.path('/');
+                    $location.path('/userprofile');
                 } else {
                     $scope.error = response.message;
                     $scope.dataLoading = false;
